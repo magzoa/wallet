4b4e9b25cbfb114f87c47b57450b2c696eacdc69
magzoaa <magzoaa@gmail.com>
2019-07-09 10:45:39 -0400

    Version php y react con prueba

---
+++ tests/Feature/WalletTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Wallet;
+use App\Transfer;
+
+class WalletTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function testGetWallet()
+    {
+
+                      // genera dentro de bd
+      $wallet=factory(Wallet::class)->create();
+      $transfers=factory(Transfer::class,3)->create([
+        'wallet_id'=>$wallet->id
+      ]);
+
+
+
+
+      $response=$this->json('GET','/api/wallet');
+
+      $response->assertStatus(200)
+                ->assertJsonStructure([
+                  'id','money','transfers'=>[
+                    '*'=>[
+                      'id','amount','description','wallet_id'
+                    ]
+                  ]
+
+                ]);
+
+      $this->assertCount(3,$response->json()['transfers']);
+
+
+
+
+
+    }
+}
