e7f7121dd2da1bb1df99914d6eeb089c6be2ebf2
magzoaa <magzoaa@gmail.com>
2019-07-09 19:35:24 -0400

    Version con prueba de insersion en bd

--- app/Transfer.php
+++ app/Transfer.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transfer extends Model
 {
-    //
+
+  public function wallet(){
+
+  return $this->belongsTo('App\Wallet');
+
+  }
+
+
+
 }
--- app/Wallet.php
+++ app/Wallet.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Wallet extends Model
 {
-    //
+
+  public function transfers(){
+
+return $this->hasMany('App\Transfer');
+
+  }
+
+
 }
